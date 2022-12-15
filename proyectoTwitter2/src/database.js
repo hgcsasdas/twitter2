0c5e639df32daecd79bed5b727a29792d78efb70
hgcsasdas <csanchezn3@alumnos.nebrija.es>
2022-12-15 14:27:44 +0100

    dos comentarios

--- proyectoTwitter2/src/database.js
+++ proyectoTwitter2/src/database.js
@@ -1,5 +1,7 @@
+//Esto lo que hace es una utilización de la librería 
+//de mongoose para guardarlo en una constante y poder utilizarla cuando quieras
 const mongoose = require('mongoose');
-
+//esto utiliza la conexión
 const { database } = require('./keys');
 
 mongoose.set('strictQuery', true)
--- proyectoTwitter2/src/public/routes/index.js
+++ proyectoTwitter2/src/public/routes/index.js
@@ -1,3 +1,4 @@
+//Esto te manda a la página de inicio
 module.exports = app =>{
     app.get('/', (req, res) =>{
         res.send('index page');
